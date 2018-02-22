33cc77f7bacd8c5afdd3befbbfeac600cc06a2ff
Kelley Jenkins <ckjenkins3@gmail.com>
2018-02-22 13:49:27 -0700

    changed button text and added create method

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -1,7 +1,21 @@
 class EmployersController < ApplicationController
   def index
-  end 
+  end
 
   def show
+  end
+
+  def new
+    @employer = Employer.new
+  end
+
+  def create
+    @employer = Employer.new(employer_params)
+    if @employer.save
+      session[:employer_id] = @employer.id
+      redirect_to employers_dashboard_path
+    else
+      render :new
+    end
   end
 end
