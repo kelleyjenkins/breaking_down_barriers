8a68f3d387ec66ef52ff46f8b5303bb77d84884b
Kelley Jenkins <ckjenkins3@gmail.com>
2018-02-22 14:12:56 -0700

    create current_employer helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-  helper_method :current_user
+  helper_method :current_user, :current_employer
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_employer
+    @current_user ||= Employer.find(session[:employer_id]) if session[:employer_id]
   end
 end
