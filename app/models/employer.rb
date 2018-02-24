3e819f1ce8f07e800813b577ba4648b6468a52f5
Kelley Jenkins <ckjenkins3@gmail.com>
2018-02-24 10:36:17 -0700

    redid employer migration

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,6 +1,5 @@
-class Employer < ActiveRecord::Base
+class Employer < ApplicationRecord
   validates_presence_of :company_name, :email, :password
   validates_uniqueness_of :company_name
   has_secure_password
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180222051754) do
+ActiveRecord::Schema.define(version: 20180224173400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20180222051754) do
     t.string "state"
     t.string "email"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
