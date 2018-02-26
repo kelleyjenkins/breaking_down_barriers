208835d0d63618c04fe03452c4e737177d1e0bad
Kelley Jenkins <ckjenkins3@gmail.com>
2018-02-25 19:33:55 -0700

    getting some styling put together

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180225225753) do
+ActiveRecord::Schema.define(version: 20180226015227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20180225225753) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "profile_pic"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
