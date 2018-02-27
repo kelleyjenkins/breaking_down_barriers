28f5410d2127381fd3f93667ad7fc54818f05ef9
Kelley Jenkins <ckjenkins3@gmail.com>
2018-02-26 20:29:27 -0700

    testing Job.near possibilities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     jobs = Job.all
+    jobs_near = Job.near([@user.profile.latitude, @user.profile.longitude], 20)
+
     if @user.profile != nil
     @job_markers = [['Me', @user.name, @user.profile.latitude, @user.profile.longitude]]
-      jobs.each do |job|
+      jobs_near.each do |job|
         @job_markers << [job.title, job.employer.company_name, job.latitude, job.longitude ]
       end
     end
