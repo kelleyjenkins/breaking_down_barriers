7302050080d417f7625b576eb6f18ef15df4d88f
Kelley Jenkins <ckjenkins3@gmail.com>
2018-02-26 18:09:14 -0700

    practicing displaying array of markers on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @user = current_user
+    # @user = current_user
+    # jobs = Job.all
+    # @job_markers = [['Me', @user.profile.latitude, @user.profile.longitude]]
+    #   jobs.each do |job|
+    #     @job_markers << [job.title, job.latitude, job.longitude ]
+    #   end
   end
 end
