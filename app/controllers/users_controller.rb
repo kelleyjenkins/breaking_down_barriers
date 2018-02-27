63584b2f644f6002e4c13d0d0f3e17808844584a
Kelley Jenkins <ckjenkins3@gmail.com>
2018-02-26 21:34:57 -0700

    employer model test passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,17 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
     jobs = Job.all
-    jobs_near = Job.near([@user.profile.latitude, @user.profile.longitude], 20)
 
     if @user.profile != nil
+      jobs_near = Job.near([@user.profile.latitude, @user.profile.longitude], 20)
+
     @job_markers = [['Me', @user.name, @user.profile.latitude, @user.profile.longitude]]
       jobs_near.each do |job|
         @job_markers << [job.title, job.employer.company_name, job.latitude, job.longitude ]
       end
     end
+    
   end
 end
--- spec/models/employer_spec.rb
+++ spec/models/employer_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Employer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has valid attributes" do
+    employer = Employer.create(company_name: "Target", street_address: "14 Elm St", city: "Denver", state: "CO", email: "target@target.com", password_digest: "test")
+
+    expect(employer.company_name).to eq("Target")
+    expect(employer.street_address).to eq("14 Elm St")
+    expect(employer.city).to eq("Denver")
+    expect(employer.state).to eq("CO")
+    expect(employer.email).to eq("target@target.com")
+    expect(employer.password_digest).to eq("test")
+  end
 end
